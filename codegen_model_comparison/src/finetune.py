d1157a5a2ae9ac5302c4b45909703cd0f6078a90
Roma Koulikov <rkoulikov@pm.me>
2023-11-15 15:20:10 -0500

    Added partial functionality to finetune.py

--- codegen_model_comparison/src/finetune.py
+++ codegen_model_comparison/src/finetune.py
@@ -1,24 +1,42 @@
 import argparse
+from functools import partial
 import logging
 import pickle
 
 import datasets
 import evaluate
+import mlflow
+import numpy as np
 import torch
 from transformers import (AutoModelForCausalLM, AutoTokenizer,
                           DataCollatorWithPadding, Trainer, TrainingArguments)
 
 
-def tokenize_function(example):
-    return tokenizer(example['text'],
-                     padding="max_length",
-                     truncation=True,
-                     max_length=500)
+def tokenize_function(example, tokenizer, batch_size, seq_length):
+    return tokenizer(
+        example['text'],
+        padding="max_length",
+        truncation=True,
+        batch_size=batch_size,
+        max_length=seq_length,
+    )
 
 
-def add_labels(example):
-    example['label'] = example['input_ids']
-    return example
+# def add_labels(example):
+#     example['label'] = example['input_ids']
+#     return example
+
+# def compute_bleu_score(preds):
+#     logits = preds.predictions[0]
+#     preds_tok = np.argmax(logits, axis=2)
+#     acts = preds.label_ids
+
+#     decode_predictions = tokenizer.batch_decode(preds_tok,
+#                                                 skip_special_tokens=True)
+#     decode_labels = tokenizer.batch_decode(acts, skip_special_tokens=True)
+
+#     res = bleu.compute(predictions=decode_predictions, references=decode_labels)
+#     return {'bleu_score': res['bleu']}
 
 
 def main(args):
@@ -45,29 +63,40 @@ def main(args):
         checkpoint, trust_remote_code=True).to(device)
 
     logger.info('Tokenize data')
-    tokenized_dataset = data.map(tokenize_function,
-                                 batched=True,
-                                 batch_size=batch_size).map(
-                                     add_labels,
-                                     batched=True,
-                                     batch_size=batch_size)
+    tokenize_partial = partial(tokenize_function,
+                               tokenizer=tokenizer,
+                               batch_size=batch_size,
+                               seq_length=seq_length)
+    tokenized_dataset = data.map(
+        tokenize_partial,
+        batched=True
+    )  #.map(add_labels, batched=True, batch_size=batch_size)
 
     # Data collator - Assembles data into batches for training
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     training_args = TrainingArguments(output_dir='trainer_' + checkpoint,
-                                      evaluation_strategy="epoch")
+                                      gradient_checkpointing=True,
+                                      evaluation_strategy="epoch",
+                                      num_train_epochs=1)
 
-    trainer = Trainer(
-        model,
-        training_args,
-        train_dataset=tokenized_dataset['train'],
-        eval_dataset=tokenized_dataset['test'],
-        data_collator=data_collator,
-        #compute_metrics=compute_bleu_score,
-        tokenizer=tokenizer)
+    bleu = evaluate.load("bleu")
+
+    logger.info('Finetune model')
+    with mlflow.start_run():
+        mlflow.transformers.autolog(log_models=False)
+        trainer = Trainer(
+            model,
+            training_args,
+            train_dataset=tokenized_dataset['train'],
+            eval_dataset=tokenized_dataset['test'],
+            data_collator=data_collator,
+            #compute_metrics=compute_bleu_score,
+            tokenizer=tokenizer)
 
     trainer.train()
+
+    trainer.save_model()
 
 
 def parse_args():
