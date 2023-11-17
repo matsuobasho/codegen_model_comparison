7a9a932c27dcdfaf4a38cdc01a6375db27caec8b
Roma Koulikov <rkoulikov@pm.me>
2023-11-17 17:17:33 -0500

    Finetune changes - Added but commented output model output - Specified correct syntax for model directory

--- codegen_model_comparison/src/finetune.py
+++ codegen_model_comparison/src/finetune.py
@@ -1,30 +1,31 @@
 import argparse
 from functools import partial
 import logging
+import os
 import pickle
 
 import datasets
 import evaluate
-import mlflow
+#import mlflow
 import numpy as np
 import torch
 from transformers import (AutoModelForCausalLM, AutoTokenizer,
                           DataCollatorWithPadding, Trainer, TrainingArguments)
 
 
-def tokenize_function(example, tokenizer, batch_size, seq_length):
+def tokenize_function(example, tokenizer, seq_length):
     return tokenizer(
         example['text'],
         padding="max_length",
         truncation=True,
-        batch_size=batch_size,
         max_length=seq_length,
     )
 
 
-# def add_labels(example):
-#     example['label'] = example['input_ids']
-#     return example
+def add_labels(example):
+    example['label'] = example['input_ids']
+    return example
+
 
 # def compute_bleu_score(preds):
 #     logits = preds.predictions[0]
@@ -44,6 +45,7 @@ def main(args):
     data_path = args.data_path
     batch_size = args.batch_size
     seq_length = args.seq_length
+    #model_dir = args.model_dir
 
     handler = logging.StreamHandler()
     logger = logging.getLogger(__name__)
@@ -65,17 +67,19 @@ def main(args):
     logger.info('Tokenize data')
     tokenize_partial = partial(tokenize_function,
                                tokenizer=tokenizer,
-                               batch_size=batch_size,
                                seq_length=seq_length)
-    tokenized_dataset = data.map(
-        tokenize_partial,
-        batched=True
-    )  #.map(add_labels, batched=True, batch_size=batch_size)
+
+    tokenized_dataset = data.map(tokenize_partial,
+                                 batched=True,
+                                 batch_size=batch_size).map(
+                                     add_labels,
+                                     batched=True,
+                                     batch_size=batch_size)
 
     # Data collator - Assembles data into batches for training
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-    training_args = TrainingArguments(output_dir='trainer_' + checkpoint,
+    training_args = TrainingArguments(output_dir='./outputs_' + checkpoint,
                                       gradient_checkpointing=True,
                                       evaluation_strategy="epoch",
                                       num_train_epochs=1)
@@ -83,20 +87,23 @@ def main(args):
     bleu = evaluate.load("bleu")
 
     logger.info('Finetune model')
-    with mlflow.start_run():
-        mlflow.transformers.autolog(log_models=False)
-        trainer = Trainer(
-            model,
-            training_args,
-            train_dataset=tokenized_dataset['train'],
-            eval_dataset=tokenized_dataset['test'],
-            data_collator=data_collator,
-            #compute_metrics=compute_bleu_score,
-            tokenizer=tokenizer)
+    #with mlflow.start_run():
+    #mlflow.transformers.autolog(log_models=False)
+    trainer = Trainer(
+        model,
+        training_args,
+        train_dataset=tokenized_dataset['train'],
+        eval_dataset=tokenized_dataset['test'],
+        data_collator=data_collator,
+        #compute_metrics=compute_bleu_score,
+        tokenizer=tokenizer)
 
     trainer.train()
 
     trainer.save_model()
+
+    model_files = os.listdir(trainer.args.output_dir)
+    print("Model files in output directory:", model_files)
 
 
 def parse_args():
@@ -105,6 +112,7 @@ def parse_args():
     parser.add_argument("--data_path", type=str)
     parser.add_argument("--batch_size", type=int)
     parser.add_argument("--seq_length", type=int)
+    #parser.add_argument("--model_dir", type=str)
     args = parser.parse_args()
 
     return args
